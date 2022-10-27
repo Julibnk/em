9c3333591431f8df529e06d4a615c41bcab30b5e
Julibnk <fernandezfernandezjulian@gmail.com>
2022-10-27 17:56:13 +0200

    Prueba commit desde windows a wsl

--- tests/core/Template/TemplateFinder.test.ts
+++ tests/core/Template/TemplateFinder.test.ts
@@ -1,3 +1,6 @@
 describe('TemplateFinder', () => {
   it('Should return a valid template', async () => {});
+  it('Should return a valid template', async () => {});
+  it('Should return a valid template', async () => {});
+  it('Should return a valid template', async () => {});
 });
