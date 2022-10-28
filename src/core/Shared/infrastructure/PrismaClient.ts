3d1bc4c6fe91b8d2254ea8266ea2d9efc8b3a6a1
Julibnk <fernandezfernandezjulian@gmail.com>
2022-10-28 11:21:53 +0200

    Prisma client config

--- src/core/Shared/infrastructure/PrismaClient.ts
+++ src/core/Shared/infrastructure/PrismaClient.ts
@@ -1,11 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
+declare global {
+  var client: PrismaClient; // eslint-disable-line no-var
+}
+
 export class PrismaClientSingleton {
   private static client: PrismaClient;
 
   static get instance(): PrismaClient {
     if (!PrismaClientSingleton.client) {
-      PrismaClientSingleton.client = new PrismaClient();
+      PrismaClientSingleton.client = global.client || new PrismaClient();
+    }
+
+    // In development the client is saved in glonal variable to prevent multiple instances beacuse of hot reloading
+    if (process.env.NODE_ENV === 'development') {
+      global.client = PrismaClientSingleton.client;
     }
 
     return PrismaClientSingleton.client;
