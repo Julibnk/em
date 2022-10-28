b3e25a7e19fcedd203815b5e1850304ba54fc809
Julibnk <fernandezfernandezjulian@gmail.com>
2022-10-28 11:55:10 +0200

    Added test for DB conection

--- tests/Shared/PrismaClient.test.ts
+++ tests/Shared/PrismaClient.test.ts
@@ -2,8 +2,13 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaClientSingleton } from '../../src/core/Shared/infrastructure/PrismaClient';
 
 describe('PrismaClient', () => {
+  const client = PrismaClientSingleton.instance;
+
   it('Should create prisma client instance', () => {
-    const client = PrismaClientSingleton.instance;
     expect(client).toBeInstanceOf(PrismaClient);
+  });
+
+  it('Should connect to the DB successfully', async () => {
+    await client.$connect();
   });
 });
