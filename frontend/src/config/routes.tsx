1ae4fefa013903db5cc009ce26bf2f6287d0b537
Julibnk <fernandezfernandezjulian@gmail.com>
2022-11-30 21:04:49 +0100

    yeeee

--- frontend/src/config/routes.tsx
+++ frontend/src/config/routes.tsx
@@ -9,33 +9,32 @@ export interface Route {
 }
 
 export const routes: Route[] = [
-  {
-    path: 'home/*',
-    component: lazy(() => import('../components/HomeScreen')),
-  },
-  {
-    path: 'message/*',
-    title: i18n.t('message', { count: 0 }) || undefined,
-    subtitle: i18n.t('message_subtitle') || undefined,
-    component: lazy(() => import('../components/MessageScreen')),
-  },
-  {
-    path: 'contact/*',
-    title: i18n.t('contact', { count: 0 }) || undefined,
-    subtitle: i18n.t('contact_subtitle') || undefined,
-
-    component: lazy(() => import('../components/ContactScreen')),
-  },
-  {
-    path: 'configuration/*',
-    title: i18n.t('configuration', { count: 0 }) || undefined,
-    subtitle: i18n.t('configuration_subtitle') || undefined,
-    component: lazy(() => import('../components/ConfigurationScreen')),
-  },
-  {
-    path: 'profile/*',
-    title: i18n.t('profile') || undefined,
-    subtitle: i18n.t('profile_subtitle') || undefined,
-    component: lazy(() => import('../components/ProfileScreen')),
-  },
+  // {
+  //   path: 'home/*',
+  //   component: lazy(() => import('../components/HomeScreen')),
+  // },
+  // {
+  //   path: 'message/*',
+  //   title: i18n.t('message', { count: 0 }) || undefined,
+  //   subtitle: i18n.t('message_subtitle') || undefined,
+  //   component: lazy(() => import('../components/MessageScreen')),
+  // },
+  // {
+  //   path: 'contact/*',
+  //   title: i18n.t('contact', { count: 0 }) || undefined,
+  //   subtitle: i18n.t('contact_subtitle') || undefined,
+  //   component: lazy(() => import('../components/ContactScreen')),
+  // },
+  // {
+  //   path: 'configuration/*',
+  //   title: i18n.t('configuration', { count: 0 }) || undefined,
+  //   subtitle: i18n.t('configuration_subtitle') || undefined,
+  //   component: lazy(() => import('../components/ConfigurationScreen')),
+  // },
+  // {
+  //   path: 'profile/*',
+  //   title: i18n.t('profile') || undefined,
+  //   subtitle: i18n.t('profile_subtitle') || undefined,
+  //   component: lazy(() => import('../components/ProfileScreen')),
+  // },
 ];
