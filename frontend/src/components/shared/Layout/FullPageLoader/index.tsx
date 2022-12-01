85600006006f687be03007a5276b8218634dc15e
Julibnk <fernandezfernandezjulian@gmail.com>
2022-12-01 13:31:58 +0100

    .

--- frontend/src/components/shared/Layout/FullPageLoader/index.tsx
+++ frontend/src/components/shared/Layout/FullPageLoader/index.tsx
@@ -3,7 +3,7 @@ import { LoadingOverlay } from '@mantine/core';
 export const FullPageLoader = () => {
   return (
     // <div style={{ position: 'relative' }}>
-    <LoadingOverlay visible={true} />
+    <LoadingOverlay overlayOpacity={0.5} visible={true} />
     // <LoadingOverlay visible={true}>{children}</LoadingOverlay>
     // </div>
   );
