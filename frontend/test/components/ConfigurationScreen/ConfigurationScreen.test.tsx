2ada716d059b98957986080a934f62c165546cec
Julibnk <fernandezfernandezjulian@gmail.com>
2022-12-02 13:50:26 +0100

    Configure vitest

---
+++ frontend/test/components/ConfigurationScreen/ConfigurationScreen.test.tsx
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react';
+// import ConfigurationScreen from '../../../src/components/ConfigurationScreen/index';
+
+describe('Configuration Screen', () => {
+  it('should do somethig', () => {
+    render(<div></div>);
+    // const a = 'Si sale es mal';
+    // console.log(a);
+    // render(<ConfigurationScreen />);
+  });
+});
