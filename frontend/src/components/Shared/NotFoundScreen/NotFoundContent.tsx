018a93f1cc27fa62eea9b1b22f50b4dba8cb34e7
Julibnk <fernandezfernandezjulian@gmail.com>
2022-12-08 15:24:31 +0100

    .

--- frontend/src/components/Shared/NotFoundScreen/NotFoundContent.tsx
+++ frontend/src/components/Shared/NotFoundScreen/NotFoundContent.tsx
@@ -1,10 +1,20 @@
 import styles from './styles.module.css';
 
+import { Title } from '@mantine/core';
+import { useTranslation } from '../../../core/Shared/hooks/useTranslation';
+import { Link } from 'react-router-dom';
+
 export const NotFoundContent = () => {
+  const t = useTranslation();
   return (
     <div className={styles.root}>
-      {/* <h3>Not found</h3> */}
+      <Title>{t('not_found_title')}</Title>
+
       <div className={styles.image} />
+
+      <Link to={'/'}>
+        <Title order={3}>&#8656; {t('not_found_subtitle')}</Title>{' '}
+      </Link>
     </div>
   );
 };
