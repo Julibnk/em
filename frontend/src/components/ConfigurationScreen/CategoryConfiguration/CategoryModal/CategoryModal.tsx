cbf8d1f5a655f5eaea8a37315ccde7f6acf77ee5
Julibnk <fernandezfernandezjulian@gmail.com>
2023-02-07 20:21:51 +0100

    .

--- frontend/src/components/ConfigurationScreen/CategoryConfiguration/CategoryModal/CategoryModal.tsx
+++ frontend/src/components/ConfigurationScreen/CategoryConfiguration/CategoryModal/CategoryModal.tsx
@@ -1,9 +1,13 @@
 import { CategoryModalState } from './categoryModalReducer';
-import { Modal, ModalProps } from '@mantine/core';
+import { Button, Group, Modal, ModalProps } from '@mantine/core';
 import { CategoryModalForm } from './CategoryModalForm';
 import { ModalTitle, ModalTitleEntity } from '../../../Shared/ModalTitle';
 import { CategoryOnlyIds } from '../../../../core/Category/Category';
 import { LoadingOverlay } from '../../../Shared/Loading';
+import { SecondaryButton } from '../../../Shared/SecondaryButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFile, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+import { useTranslation } from '../../../Shared/hooks/useTranslation';
 
 export interface Props {
   state: CategoryModalState;
@@ -13,6 +17,7 @@ export interface Props {
 
 export const CategoryModal = ({ state, handleClose, handleSubmit }: Props) => {
   const { opened, mode, category, allTemplates, loading } = state;
+  const t = useTranslation();
 
   const modalProps: ModalProps = {
     opened: opened,
@@ -28,16 +33,29 @@ export const CategoryModal = ({ state, handleClose, handleSubmit }: Props) => {
 
   if (!category) return null;
 
+  const mainButtonIcon = mode === 'CREATE' ? faFile : faFloppyDisk;
+  const mainButtonText = mode === 'CREATE' ? t('create') : t('save');
+
   return (
     <Modal {...modalProps}>
       <LoadingOverlay loading={loading} />
       <CategoryModalForm
-        handleClose={handleClose}
+        // handleClose={handleClose}
         handleSubmit={handleSubmit}
-        mode={mode}
+        // mode={mode}
         category={category}
         allTemplates={allTemplates}
       />
+      <Group position='right' mt='md'>
+        <SecondaryButton onClick={handleClose}>{t('cancel')}</SecondaryButton>
+        <Button
+          type='submit'
+          form='category-form'
+          leftIcon={<FontAwesomeIcon icon={mainButtonIcon} />}
+        >
+          {mainButtonText}
+        </Button>
+      </Group>
     </Modal>
   );
 };
--- frontend/src/components/ConfigurationScreen/CategoryConfiguration/CategoryModal/CategoryModalForm.tsx
+++ frontend/src/components/ConfigurationScreen/CategoryConfiguration/CategoryModal/CategoryModalForm.tsx
@@ -1,34 +1,18 @@
 import { useForm } from '@mantine/form';
-import {
-  Button,
-  Group,
-  MultiSelect,
-  SelectItem,
-  TextInput,
-} from '@mantine/core';
-import { SecondaryButton } from '../../../Shared/SecondaryButton';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFile } from '@fortawesome/free-regular-svg-icons';
-import { faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
+import { MultiSelect, SelectItem, TextInput } from '@mantine/core';
 import { Category, CategoryOnlyIds } from '../../../../core/Category/Category';
-import { Nullable } from '../../../../core/Shared/Nullable';
 import { useTranslation } from '../../../Shared/hooks/useTranslation';
 import { Template } from '../../../../core/Template/Template';
-import { ModalMode } from '../../../Shared/ModalTitle';
 
 export interface Props {
-  handleClose: () => void;
   handleSubmit: (category: CategoryOnlyIds) => void;
   category: Category;
   allTemplates: Template[];
-  mode: Nullable<ModalMode>;
 }
 
 export const CategoryModalForm = ({
-  handleClose,
   handleSubmit,
   category,
-  mode,
   allTemplates,
 }: Props) => {
   const t = useTranslation();
@@ -40,16 +24,16 @@ export const CategoryModalForm = ({
     },
   });
 
-  const mainButtonIcon = mode === 'CREATE' ? faFile : faFloppyDisk;
-  const mainButtonText = mode === 'CREATE' ? t('create') : t('save');
-
   const multiSelectData: SelectItem[] = allTemplates.map((template) => ({
     value: template.id,
     label: template.name,
   }));
 
   return (
-    <form onSubmit={form.onSubmit((values) => handleSubmit(values))}>
+    <form
+      id='category-form'
+      onSubmit={form.onSubmit((values) => handleSubmit(values))}
+    >
       <TextInput required label={t('name')} {...form.getInputProps('name')} />
       <TextInput
         label={t('description')}
@@ -60,15 +44,6 @@ export const CategoryModalForm = ({
         label={t('template', { plural: true })}
         {...form.getInputProps('templates')}
       />
-      <Group position='right' mt='md'>
-        <SecondaryButton onClick={handleClose}>{t('cancel')}</SecondaryButton>
-        <Button
-          type='submit'
-          leftIcon={<FontAwesomeIcon icon={mainButtonIcon} />}
-        >
-          {mainButtonText}
-        </Button>
-      </Group>
     </form>
   );
 };
